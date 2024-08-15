c8a76f2165edfff1ef5b5902cbaa4b6ca225e0d2
Matheus Aguiar <matheus.pit@gmail.com>
2024-08-14 23:53:27 -0300

    fixup! add tests

---
+++ test/libsolidity/semanticTests/getters/transient_value_types multi_frame_call.sol
@@ -0,0 +1,27 @@
+contract C {
+}
+
+contract D {
+    int8 public transient x;
+
+    function f() public returns(int8) {
+        x = -1;
+        return h();
+    }
+    function g() public {
+        x = x - 1;
+    }
+    function h() public returns(int8) {
+        g();
+        return this.x();
+    }
+    
+}
+// ====
+// EVMVersion: >=cancun
+// compileViaYul: false
+// ----
+// x() -> 0
+// f() -> -2
+// h() -> -1
+// x() -> 0
